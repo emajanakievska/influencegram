054de348560a027a64401861ce2fdfefe1ae0874
FilipIlievski <ilievski.filip.mk@gmail.com>
2018-09-24 14:44:36 +0200

    [FIX] Adding production keys

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,6 @@
 module.exports = {
     instagramClientID: process.env.INSTAGRAM_CLIENT_ID,
     instagramClientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+    mongoUri: process.env.MONGO_URI
 };
